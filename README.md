b5ef324dc55b5e62ce5c2624994c4553895e728d
Jan Kaßel <jankassel@hotmail.de>
2015-02-16 11:35:27 +0100

    Added piping stderr of lessc in case of any errors, refactoring

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A tool written in [Go](http://golang.org), which keeps track on file changes on 
 
 ### Prerequisites
 
-Make sure to have [Go setup correctly](http://golang.org/doc/install) and did the [$GOPATH thing](https://github.com/golang/go/wiki/GOPATH). LESS (especially `lessc`) has to be installed and made available in your $PATH as well (preferably via [`npm -g`](https://www.npmjs.com/package/less)).
+Make sure to have [Go installed and setup correctly](http://golang.org/doc/install) (I'm using Go 1.4.1 – no other versions are tested) and did the [$GOPATH thing](https://github.com/golang/go/wiki/GOPATH). LESS (especially `lessc`) has to be installed and made available in your $PATH as well (preferably via [`npm -g`](https://www.npmjs.com/package/less)).
 
 ### Install
 
@@ -17,7 +17,9 @@ To install `lessc-watch` via Go's package manager, try `go get github.com/jankas
 lessc-watch [-x] input-directory [output-directory]
 ```
 
-The listener will watch for file changes in the `input-directory`. `output-directory` states where to put compiler output (i.e. the CSS). If omitted, all output will go into the same directory as `input-directory`. `-x` will enable compression, passing the same parameter to `lessc`. 
+The listener will watch for file changes in the `input-directory`. `output-directory` states where to put compiler output (i.e. the CSS). If omitted, all output will go into the same directory as `input-directory`. `-x` will enable compression, passing the same parameter to `lessc`.
+
+`lessc-watch` will tell you when and which file has been compiled, and if any errors occured.
 
 ### Notes
 
--- lessc-watch.go
+++ lessc-watch.go
@@ -1,6 +1,8 @@
 package main
 
 import(
+  "bytes"
+  "errors"
   "fmt"
   "log"
   "os"
@@ -10,6 +12,11 @@ import(
   "strings"
   "gopkg.in/fsnotify.v1"
 )
+
+type Settings struct {
+  indir, outdir string
+  compress bool
+}
 
 func directoriesExist(paths ...string) (exist bool, err error) {
   for _, path := range paths {
@@ -30,14 +37,23 @@ func evaluateCompressionFlag(compress bool) string {
   }
 }
 
-func compileLessFile(input string, output string, compress bool) error {
+func compileLessFile(input string, output string, compress bool) (err error, errstr string) {
   cmd := exec.Command("lessc", evaluateCompressionFlag(compress), input, output)
-  err := cmd.Start()
+
+  var stderr bytes.Buffer
+  cmd.Stderr = &stderr
+
+  err = cmd.Start()
   if err != nil {
-    return err
+    return err, ""
   }
 
-  return cmd.Wait()
+  err = cmd.Wait()
+  if (err != nil) {
+    return err, stderr.String()
+  } else {
+    return err, ""
+  }
 }
 
 func getBaseName(filename string) string {
@@ -53,40 +69,77 @@ func isFile(path string) (isfile bool, err error) {
   return true, nil
 }
 
-func main() {
-  args := os.Args[1:]
+func watch(watcher *fsnotify.Watcher, settings *Settings) {
+  for {
+    select {
+      case event := <-watcher.Events:
+        if event.Op&fsnotify.Write == fsnotify.Write {
+          filePath := event.Name
+          if isfile, err := isFile(filePath); err == nil && isfile && filepath.Ext(filePath) == ".less" {
+            in := filePath
+            name := path.Base(filePath)
+            base := getBaseName(name)
+            out := settings.outdir + "/" + base + ".css"
 
-  if len(args) == 0 || (len(args) == 1 && args[0] == "-x") {
-    fmt.Println("Usage: lessc-watch [-x] input-path [output-path]")
-    os.Exit(0)
+            if err, errstr := compileLessFile(in, out, settings.compress); err == nil {
+              log.Printf("Compiled %s\n", name)
+            } else {
+              log.Printf("An error occured (%s):\n%s", fmt.Sprint(err), errstr)
+            }
+          }
+        }
+
+      case err := <-watcher.Errors:
+        log.Printf("Error: %s\n", err)
+    }
   }
+}
 
+func getargs(args []string) (s *Settings, err error) {
   compress := false
   if args[0] == "-x" {
     compress = true
     args = args[1:]
   }
 
-  input, err := filepath.Abs(args[0])
+  indir, err := filepath.Abs(args[0])
   if err != nil {
-    log.Fatal(err)
+    return nil, err
   }
 
-  output := input
+  outdir := indir
   if len(args) > 1 {
-    output, err = filepath.Abs(args[1])
+    outdir, err = filepath.Abs(args[1])
 
     if err != nil {
-      log.Fatal(err)
+      return nil, err
     }
   }
 
-  if exist, err := directoriesExist(input, output); !exist || err != nil {
+  if exist, err := directoriesExist(indir, outdir); !exist || err != nil {
     if !exist {
-      log.Fatal("Error: Input and/or output directories do not exist.")
-    } else if err != nil {
-      log.Fatal(err)
-    }
+      err = errors.New("Error: Input and/or output directories do not exist.")
+    } else {}
+
+    return nil, err
+  }
+
+  return &Settings{
+    indir: indir,
+    outdir: outdir,
+    compress: compress }, nil
+}
+
+func main() {
+  args := os.Args[1:]
+  if len(args) == 0 || (len(args) == 1 && args[0] == "-x") {
+    fmt.Println("Usage: lessc-watch [-x] input-path [output-path]")
+    os.Exit(0)
+  }
+
+  s, err := getargs(args)
+  if (err != nil) {
+    log.Fatal(err)
   }
 
   watcher, err := fsnotify.NewWatcher()
@@ -96,30 +149,9 @@ func main() {
   defer watcher.Close()
 
   done := make(chan bool)
-  go func() {
-    for {
-      select {
-        case event := <-watcher.Events:
-          if event.Op&fsnotify.Write == fsnotify.Write {
-            filePath := event.Name
-            if isfile, err := isFile(filePath); err == nil && isfile && filepath.Ext(filePath) == ".less" {
-              in := filePath
-              name := path.Base(filePath)
-              base := getBaseName(name)
-              out := output + "/" + base + ".css"
+  go watch(watcher, s)
 
-              if err := compileLessFile(in, out, compress); err == nil {
-                log.Printf("Compiled %s\n", name)
-              } else {
-                log.Print(err)
-              }
-            }
-          }
-      }
-    }
-  }()
-
-  err = watcher.Add(input)
+  err = watcher.Add(s.indir)
   if err != nil {
     log.Fatal(err)
   }
